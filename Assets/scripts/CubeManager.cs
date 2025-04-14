7fdc2ab62fbe8a106594e8c21b3269cff9bd8b7a
LeeTaeHyun-05 <thrhkwpdyd@gmail.com>
2025-04-14 16:06:11 +0900

    [Feat] if문, for문, 배열을 활용해서 Tile생성

---
+++ Assets/scripts/CubeManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public CubeGenerator[] generatedCubes = new CubeGenerator[5];       //클래스 배열
+
+    public float timer = 0.0f;                                          //시간 타이머 설정 float
+    public float interval = 3.0f;                                       //3초마다 땅 생성
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= interval)
+        {
+            RandomizeCubeActivation();
+            timer = 0.0f;
+        }
+    }
+
+    public void RandomizeCubeActivation()           
+    {
+        for (int i = 0; i < generatedCubes.Length; i++)                 //각 큐브를 랜덤하게 활성
+        {
+            int randomNum = Random.Range(0, 2);                         //랜덤 = 0또는 1 지역 변수 선언 50% 확률로 땅 생성 
+            if (randomNum == 1)
+            {
+                generatedCubes[i].GenCube();                            //큐브 클래스의 생성 함수를 호출 한다.
+
+            }
+
+        }
+    }
+}
